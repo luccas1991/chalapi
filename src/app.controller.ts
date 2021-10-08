441273eb494e30191d554b0a90648b4571f47dc5
lucs <lucas.s.lucente@gmail.com>
2021-10-08 20:06:42 -0300

    date

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { addDays, addSeconds } from 'date-fns'
 
 @Controller()
 export class AppController {
@@ -13,12 +14,14 @@ export class AppController {
         iconUrl: "images/get_started48.png",
         title: "Test Message",
         message: "You are awesome!",
+        date: addDays(new Date(),3)
       },
       {
         id: "alarma_2",
         iconUrl: "images/get_started48.png",
         title: "Test Message 2",
         message: "You are awesome!",
+        date: addSeconds(new Date(),6)
       }
     ];
   }
