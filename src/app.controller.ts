a611fa6a5b6c0c2ff950bc47e114f0027fe1f686
lucs <lucas.s.lucente@gmail.com>
2021-10-08 19:35:52 -0300

    mock

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  test(): any{
-    return { prop:"valor"}
+  test(): any {
+    return [
+      {
+        id: "alarma_1",
+        iconUrl: "images/get_started48.png",
+        title: "Test Message",
+        message: "You are awesome!",
+      },
+      {
+        id: "alarma_2",
+        iconUrl: "images/get_started48.png",
+        title: "Test Message 2",
+        message: "You are awesome!",
+      }
+    ];
   }
 }
