bf628a232a10581c5023cd0d854f0149c96c1556
lucs <lucas.s.lucente@gmail.com>
2021-11-02 22:27:59 -0300

    fix: guards on post recipe

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Request, Get, Post } from '@nestjs/common';
+import { Body, Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/createRecipe.dto';
 import { RecipeService } from './recipe.service';
+import { JWTAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller('recipe')
 export class RecipeController {
@@ -12,6 +13,7 @@ export class RecipeController {
         return this.recipeService.findAll()
     }
 
+    @UseGuards(JWTAuthGuard)
     @Post()
     create(@Request() { user }, @Body() createRecipeDto: CreateRecipeDto){
         createRecipeDto.author = user.name
